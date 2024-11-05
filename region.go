f162ca5ec3b79c7c91dd9328144a550a8d166a1c
shiyang he <2448768360@qq.com>
2024-11-05 06:30:20 +0000

    parser/region: fix typos
    Signed-off-by: shiyang he <2448768360@qq.com>

--- region.go
+++ region.go
@@ -8,8 +8,8 @@ const (
 )
 
 const (
-	USPREFIX = "http://www.amazon.com"
-	UKPREFIX = "http://www.amazon.co.uk"
-	DEPREFIX = "http://www.amazon.de"
-	FRPREFIX = "http://www.amazon.fr"
+	US_PREFIX = "https://www.amazon.com"
+	UK_PREFIX = "https://www.amazon.co.uk"
+	DE_PREFIX = "https://www.amazon.de"
+	FR_PREFIX = "https://www.amazon.fr"
 )
