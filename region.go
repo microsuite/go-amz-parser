9a70fe7bbfd69998b7742729175a0427ecabbb4e
shiyang he <2448768360@qq.com>
2024-11-05 06:13:37 +0000

    parser/region: add prefix
    Signed-off-by: shiyang he <2448768360@qq.com>

--- region.go
+++ region.go
@@ -6,3 +6,10 @@ const (
 	DE = "de-de"
 	FR = "fr-fr"
 )
+
+const (
+	USPREFIX = "http://www.amazon.com"
+	UKPREFIX = "http://www.amazon.co.uk"
+	DEPREFIX = "http://www.amazon.de"
+	FRPREFIX = "http://www.amazon.fr"
+)
